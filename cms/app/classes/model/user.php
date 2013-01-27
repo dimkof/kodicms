cf8410ed00190346602e5ea0771de22f0638e554
Dmitry <dmitry@dmstudio.pro>
2013-01-27 11:36:35 +0200

    Update cms/app/classes/model/user.php
    Подправил валидацию количества символов в пароле.
    При установке написано "Не менее 5 символов.",  
    а правило бралось со стандартной модели Model_Auth_User, где ограничение 8

--- cms/app/classes/model/user.php
+++ cms/app/classes/model/user.php
@@ -106,4 +106,17 @@ class Model_User extends Model_Auth_User {
 		$this->email = $new_email;
 		return $this->update();
 	}
+
+	/**
+	 * Password validation for plain passwords.
+	 *
+	 * @param array $values
+	 * @return Validation
+	 */
+	public static function get_password_validation($values)
+	{
+		return Validation::factory($values)
+			->rule('password', 'min_length', array(':value', 5))
+			->rule('password_confirm', 'matches', array(':validation', ':field', 'password'));
+	}
 }
